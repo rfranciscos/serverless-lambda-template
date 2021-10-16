cb566aa1e6ea9d8502c36ac4c9d3ed8216ca984b
Renan Soares Francisco <renan.soares.francisco@gmail.com>
2021-10-16 17:13:11 -0300

    adding dinamic variables in serverless.ts

--- serverless.ts
+++ serverless.ts
@@ -1,10 +1,11 @@
 import type { AWS } from '@serverless/typescript';
-
+// @ts-ignore
 import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
   service: 'lambda-template',
   frameworkVersion: '2',
+  variablesResolutionMode: '20210326',
   custom: {
     webpack: {
       includeModules: true,
@@ -14,9 +15,12 @@ const serverlessConfiguration: AWS = {
 
     }
   },
-  plugins: ['serverless-webpack'],
+  plugins: ['serverless-webpack', 'serverless-dotenv-plugin', 'serverless-offline'],
   provider: {
     name: 'aws',
+    // @ts-ignore
+    region: '${opt:region, \'us-west-2\'}',
+    stage: '${opt:stage, \'staging\'}',
     runtime: 'nodejs14.x',
     apiGateway: {
       minimumCompressionSize: 1024,
