791b2e3c2de9de4201d46bc3a81942cd151bfb8b
Renan Soares Francisco <renan.soares.francisco@gmail.com>
2021-10-16 17:19:26 -0300

    Remove .idea directory

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -6,7 +6,7 @@ import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world! ---${process.env.AWS_NODEJS_CONNECTION_REUSE_ENABLED}`,
     event,
   });
 }
